22356eade35c14e6ec712c53bac21d37d0c562e7
andyssites <127164682+andyssites@users.noreply.github.com>
2024-06-25 06:51:16 -0700

    Update toolbar.js

--- js/toolbar.js
+++ js/toolbar.js
@@ -1,5 +1,14 @@
 const targetEl = document.querySelector(".toolbar_target");
 
 const loadSnippet = number => {
-  // fetch
+  fetch("../src/toolbar.html")
+    .then(res => {
+      if (res.ok){
+        return res.text();
+      }
+      })
+      .then(htmlSnippet => {
+        targetEl.innerHTML = htmlSnippet;
+
+      })
 }
